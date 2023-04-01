5cd2e0c85db1aebaaaebefebcff0cfbcc620ed4f
fabriciofbi <fabricio_fbi@hotmail.com>
2023-04-01 16:26:28 -0300

    Criando Classe Livros

--- Livros/models.py
+++ Livros/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Livros(models.Model):
+    titulo = models.CharField(max_length=100)
+    ano = models.IntegerField(max_length=4)
+    edicao = models.IntegerField(max_length=2)
+    qtd_disponivel = models.IntegerField(max_length=5)
+    data_cadastro = models.DateTimeField()
