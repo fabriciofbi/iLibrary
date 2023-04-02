b0caec7d7c853ae0e09be7d42aa8d434bae2297d
fabriciofbi <fabricio_fbi@hotmail.com>
2023-04-02 17:43:19 -0300

    Alterando Aplicações (Criando aplicação Cadastro)

---
+++ cadastro/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from cadastro.models import Editoras, Alunos, Livros, Categorias, Autores
+
+class EditorasAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cidade')
+    list_filter = ['cidade']
+
+
+admin.site.register(Editoras, EditorasAdmin)
+
+class AlunosAdmin(admin.ModelAdmin):
+    list_display = ('ra', 'nome', 'email', 'celular', 'cidade')
+    list_filter = ['sala']
+
+admin.site.register(Alunos, AlunosAdmin)
+
+class LivrosAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'ano', 'edicao', 'qtd_disponivel')
+    list_filter = ['ano']
+
+admin.site.register(Livros, LivrosAdmin)
+admin.site.register(Categorias)
+admin.site.register(Autores)
---
+++ cadastro/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from cpf_field.models import CPFField
+from validate_email import validate_email
+from django.core.validators import MinValueValidator
+
+# Create your models here.
+class Editoras(models.Model):
+    nome = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=50)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Editora'
+
+    def __str__(self):
+        return self.nome
+
+class Alunos(models.Model):
+    nome = models.CharField(max_length=100, blank=True)
+    ra = models.IntegerField(unique=True)
+    cpf = CPFField(error_messages={'invalid': 'CPF inválido'}, unique=True, blank=True, max_length=11)
+    endereco = models.CharField(max_length=100, blank=True)
+    bairro = models.CharField(max_length=50, blank=True)
+    cidade = models.CharField(max_length=50, blank=True)
+    estado = models.CharField(max_length=2, blank=True)
+    celular = models.CharField(max_length=12, blank=True)
+    email = models.EmailField(max_length=100, blank=True, unique=True)
+    sala = models.CharField(max_length=10, blank=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        verbose_name = 'Aluno'
+
+    def __str__(self):
+        return self.nome
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        if self.email and not validate_email(self.email):
+            raise ValidationError('Endereço de email inválido.')
+
+class Autores(models.Model):
+    nome = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return self.nome
+
+class Categorias(models.Model):
+    nome = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'Categorias'
+
+    def __str__(self):
+        return self.nome
+
+class Livros(models.Model):
+    titulo = models.CharField(max_length=100)
+    ano = models.IntegerField(validators=[MinValueValidator(1)])
+    edicao = models.IntegerField()
+    qtd_disponivel = models.IntegerField()
+    autor = models.ForeignKey(Autores, on_delete=models.CASCADE)
+    editora = models.ForeignKey(Editoras, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=16, unique=True)
+    imagem = models.ImageField(upload_to='static/img/livros/', blank=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Livro'
