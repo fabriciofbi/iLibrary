d126c7553436e0da42663a1c8eb6f45726d4b505
luanalmeida18 <luanfelipe.lfpa@gmail.com>
2023-04-07 20:10:48 -0300

    Alterações Base HTML

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, teste
+from .views import index, teste, autores
 
 urlpatterns = [
     path('', index, name='index'),
     path('teste/', teste, name='teste'),
+    path('autores/', autores, name='autores'),
 ]
