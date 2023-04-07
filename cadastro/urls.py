8c91012369ba98b1d83a6e9df4851ebe15a2a5d3
luanalmeida18 <luanfelipe.lfpa@gmail.com>
2023-04-07 20:25:22 -0300

    Alterações Paginacao em Base Html

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import index, teste, autores
+from .views import index, livros, autores, editoras, categorias, contato, teste
 
 urlpatterns = [
     path('', index, name='index'),
-    path('teste/', teste, name='teste'),
+    path('livros/', livros, name='livros'),
     path('autores/', autores, name='autores'),
+    path('editoras/', editoras, name='editoras'),
+    path('categorias/', categorias, name='categorias'),
+    path('contato/', contato, name='contato'),
+    path('teste/', teste, name='teste'),
 ]
